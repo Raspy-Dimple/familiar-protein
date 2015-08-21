15bf4cd8ad79e3835c72cbe0e61176b44ce211a2
lisatsewang <lisatsewang@gmail.com>
2015-08-20 18:41:59 -0700

    saving unique answer for a question only when a user is logged in

--- server/answers/answerController.js
+++ server/answers/answerController.js
@@ -1,13 +1,23 @@
 var Answer = require('./answerModel');
 
 var saveAnswer = function(req, res, next) {
-	console.log("saveAnswer req.body: ", req.body);
-	var newAnswer = new Answer(req.body);
-	newAnswer.save(function(err, data) {
+	Answer.findOne({questionID: req.body.questionID, answer: req.body.answer})
+	.exec(function(err, data) {
 		if (err) {
-			console.log("Add answer error: ", err);
+			console.log("FindOne answer error: ", err);
 		} else {
-			console.log("Add answer success: ", data);
+			if (data === null) {
+				var newAnswer = new Answer(req.body);
+				newAnswer.save(function(err, data) {
+					if (err) {
+						console.log("Save answer error: ", err);
+					} else {
+						console.log("Add answer success: ", data);
+					}
+				});
+			} else {
+				console.log("Same answer same question already exist");
+			}
 		}
 	});
 }
